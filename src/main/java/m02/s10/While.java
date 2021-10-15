99626e058552c32c8c5fde1cf42ad6fbf1ba652a
Asia Perazza <asiaperazza@gmail.com>
2021-10-15 10:20:22 +0200

    new works

--- src/main/java/m02/s10/While.java
+++ src/main/java/m02/s10/While.java
@@ -4,8 +4,18 @@ public class While {
     public static void main(String[] args) {
         int i = 0;
         while (i < args.length) {
-            System.out.println("Arg " + i + ": " + args[i]);
+//        	String s = " The argument " + i + " is "+ args[i];
+        	String s = " The argument ";
+        	s = s + i;
+        	s = s + " is ";
+        	s = s + args [i];
+        	
+            System.out.println(s);
+            System.out.println("The argument " + i + " is " + args [i]);
             i += 1;
+            // i++;
+            // ++i;
+            // i = i + 1;
         }
 
         System.out.println("done!");
